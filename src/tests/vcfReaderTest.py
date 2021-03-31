014e2b6e1c347d7a26dc540020edd522463f798f
agranadosb <agranadosb55199@outlook.com>
2021-03-31 18:10:15 +0200

    feat(vcf): add tests o test prefix and suffix methods

--- src/tests/vcfReaderTest.py
+++ src/tests/vcfReaderTest.py
@@ -1,16 +1,39 @@
 from src.vcf.vcfReader import VcfMutationsReader
 
+results = [
+    ('', 'T', 'ATCAA'),
+    ('T', 'A', 'TCAAT'),
+    ('TA', 'T', 'CAATG'),
+    ('TAT', 'C', 'AATGC'),
+    ('TATC', 'A', 'ATGCC'),
+    ('TATCA', 'A', 'TGCCT'),
+    ('GAGGG', 'C', 'ATAAC'),
+    ('AGGGC', 'A', 'TAACA'),
+    ('GGGCA', 'T', 'AACAT'),
+    ('GGCAT', 'A', 'ACATC'),
+    ('AGGTA', 'T', 'CTAAT'),
+    ('GGTAT', 'C', 'TAAT'),
+    ('GTATC', 'T', 'AAT'),
+    ('TATCT', 'A', 'AT'),
+    ('ATCTA', 'A', 'T'),
+    ('TCTAA', 'T', '')
+]
+
 
 def test_get_seq_by_chr_pos():
     reader = VcfMutationsReader(
         '/opt/UPV/TFG/src/tests/vcfTest.vcf',
         '/opt/UPV/TFG/src/tests/test.fa.gz'
     )
-    for i in reader.getVcf():
+    for z in zip(reader.getVcf(), results * 3):
+        i = z[0]
+        j = z[1]
         reference = i.REF
         reference_seq_fasta = reader.get_seq_by_chr_pos(i.CHROM, i.POS, 5, 5)
 
+        print(i.CHROM)
         print(reference)
         print(reference_seq_fasta)
-        print()
+        assert j[0] == reference_seq_fasta[0]
         assert reference == reference_seq_fasta[1]
+        assert j[2] == reference_seq_fasta[2]
