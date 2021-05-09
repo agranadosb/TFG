871b5319a94698af2358b5f777a5c0e40c7b2a08
Alejandro Granados <alejandro@dekalabs.com>
2021-05-09 23:41:14 +0200

    feat(ktss): add comments and tests

--- src/model/ktssModel.py
+++ src/model/ktssModel.py
@@ -11,10 +11,23 @@ from src.parser.extendedParser import ExtendedParserVcf
 class KTSSModel(AbstractModel):
     def __init__(self):
         self.model = False
-        self.parser = ExtendedParserVcf
-        self.trainer = "k-tt"
+        self.parser_class = ExtendedParserVcf
+        self.trainer_name = "ktt"
 
     def state_in_list(self, state, lst):
+        """Checks if a state is in a list
+
+        Parameters
+        ----------
+        state: tuple
+            State to be searched (s0, s1, s2)
+        lst: list
+            list where the state will be searched
+
+        Returns
+        -------
+        True if the state is in the list, false either
+        """
         for state_list in lst:
             if (
                 state_list[0] == state[0]
@@ -25,6 +38,20 @@ class KTSSModel(AbstractModel):
         return False
 
     def generate_sigma(self, alphabet, k):
+        """Generates sigma for a k given, for example, if k = 0 and alphabet = [A, B]
+        it generates [A, B, AA, AB, BA, BB]
+
+        Parameters
+        ----------
+        alphabet: list
+            alphabet to generate sigma
+        k: int
+            length of the maximum strings of sigma
+
+        Returns
+        -------
+        sigma
+        """
         aux = 1
         words = [] + alphabet
         while aux < k:
@@ -34,12 +61,55 @@ class KTSSModel(AbstractModel):
         return words
 
     def get_prefix(self, string, k):
+        """Returns the prefix of a string
+
+        Parameters
+        ----------
+        string: str
+            Strong where the prefix will be getted
+        k: int
+            Length of the prefix - 1
+
+        Returns
+        -------
+        The prefix of the string
+        """
         return string[: k - 1]
 
-    def get_sufix(self, string, k):
+    def get_suffix(self, string, k):
+        """Returns the suffix of a string
+
+        Parameters
+        ----------
+        string: str
+            String where the suffix will be getted
+        k: int
+            Length of the suffix - 1
+
+        Returns
+        -------
+        The suffix of the string
+        """
+        if k >= len(string):
+            return string
         return string[len(string) - k + 1 :]
 
     def get_infixes(self, string, k):
+        """Generates all the possible infixes of a string for a lengt k given
+
+        Parameters
+        ----------
+        string: str
+            String where the infixes will be getted
+        k: int
+            Length of the infixes
+
+        Rreturns
+        --------
+        List of all the infixes
+        """
+        if k >= len(string):
+            return [string]
         ini = 0
         fin = k
         result = []
@@ -50,6 +120,26 @@ class KTSSModel(AbstractModel):
         return result
 
     def training(self, samples, k):
+        """Generates a ktss model from the samples and a k given
+
+        Parameters
+        ----------
+        samples: list
+            List of samples to training the model
+        k: int
+            Parameter of the ktss model
+
+        Returns
+        -------
+        {
+            "states": states,
+            "alphabet": alphabet,
+            "transitions": transitions,
+            "initial_state": initial_state,
+            "final_states": final_states,
+            "not_allowed_segments": not_allowed_segments,
+        }
+        """
         alphabet = list(set(functools.reduce(operator.add, samples)))
 
         greater_or_equal_than_k = []
@@ -61,18 +151,18 @@ class KTSSModel(AbstractModel):
                 lower_than_k.append(sample)
 
         prefixes = [] + lower_than_k
-        sufixes = [] + lower_than_k
+        suffixes = [] + lower_than_k
         infixes = []
         for sample in greater_or_equal_than_k:
-            prefixes.append(self.get_prefix(sample))
-            sufixes.append(self.get_sufix(sample))
-            infixes += self.get_infixes(sample)
+            prefixes.append(self.get_prefix(sample, k))
+            suffixes.append(self.get_suffix(sample, k))
+            infixes += self.get_infixes(sample, k)
 
         prefixes = set(prefixes)
-        sufixes = set(sufixes)
+        suffixes = set(suffixes)
         infixes = set(infixes)
 
-        sigma_k = self.generate_sigma()
+        sigma_k = self.generate_sigma(alphabet, k)
         not_allowed_segments = set(sigma_k) - set(infixes)
 
         q = [""]
@@ -112,6 +202,23 @@ class KTSSModel(AbstractModel):
             "alphabet": set(alphabet),
             "transitions": s,
             "initial_state": q0,
-            "final_states": sufixes,
+            "final_states": suffixes,
             "not_allowed_segments": not_allowed_segments,
         }
+
+        return self.model
+
+    def parser(self):
+        return self.parser_class
+
+    def trainer(self):
+        return self.training
+
+    def model(self):
+        return self.model
+
+    def saver(self):
+        pass
+
+    def loader(self):
+        pass
